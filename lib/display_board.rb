379d99b8cd2060fa52792ea3010b968bd05bfbf6
Sean Langton <slangton1@gmail.com>
2018-10-03 01:42:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(sq1,sq2,sq3,sq4,sq5,sq6,sq7,sq8,sq9)
-  puts " #{{sq1}} | #{{sq2}} | #{{sq3}} "
+  puts " #{sq1} | #{sq2} | #{sq3} "
   puts "-----------"
-  puts " #{{sq4}} | #{{sq5}} | #{{sq6}} "
+  puts " #{sq4} | #{sq5} | #{sq6} "
   puts "-----------"
-  puts " #{{sq7}} | #{{sq8}} | #{{sq9}} "
+  puts " #{sq7} | #{sq8} | #{sq9} "
 end
